978dec5e928c5180fe441712f8e9390dc44db248
David Dreschner <github-2017@dreschner.net>
2023-06-26 11:44:56 +0200

    Fix broken build on non-ble-central devices

--- app/include/zmk/backlight.h
+++ app/include/zmk/backlight.h
@@ -16,7 +16,7 @@ int zmk_backlight_off();
 int zmk_backlight_toggle();
 bool zmk_backlight_is_on();
 
-int zmk_backlight_update_vals(struct backlight_state new_state);
+int zmk_backlight_update_vals(struct backlight_state *new_state);
 int zmk_backlight_set_brt(uint8_t brightness);
 uint8_t zmk_backlight_get_brt();
 uint8_t zmk_backlight_calc_brt(int direction);
