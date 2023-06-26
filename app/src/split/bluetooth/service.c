333efca68de2772c260a46b08b5c03724b407e75
David Dreschner <github-2017@dreschner.net>
2023-06-26 12:15:40 +0200

    Fix broken build on non-ble-central devices

--- app/src/split/bluetooth/service.c
+++ app/src/split/bluetooth/service.c
@@ -96,7 +96,7 @@ static ssize_t split_svc_update_led(struct bt_conn *conn, const struct bt_gatt_a
     // 1: We've gotten all the position/state/param data.
     // 2: We have a null terminated string for the behavior device label.
     if ((end_addr == sizeof(struct zmk_split_update_led_data))) {
-        struct zmk_periph_led state = {.layer = payload->layer, .indicators = payload->indicators};
+        struct zmk_periph_led state = {.layer = &payload->layer, .indicators = &payload->indicators};
         zmk_rgb_underglow_set_periph(state);
         LOG_DBG("Update leds with params %d and %d", state.layer, state.indicators);
     }
